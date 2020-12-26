6a1810986a7892b8b07da15a5448e68af3281ff2
Muhammad Rabiul Alam <bm.rabiulalam@gmail.com>
2020-12-26 18:19:35 +0600

    Some minor fixes

--- ecommerce-back-end/src/routes/category.js
+++ ecommerce-back-end/src/routes/category.js
@@ -2,13 +2,13 @@ const express = require('express');
 const { adminMiddleware, requireSignin } = require('../common-middleware');
 const { addCategory, getCategory } = require('../controllers/category');
 const router = express.Router();
-const multer = require('multer');
 const shortid = require('shortid');
 const path = require('path');
+const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, path.join(path.dirname(__dirname), 'uploads'))
+        cb(null, path.join(path.dirname(__dirname), 'uploads'));
     },
     filename: function(req, file, cb){
         cb(null, shortid.generate() + '-' + file.originalname);
